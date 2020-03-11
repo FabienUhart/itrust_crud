d69517dd9104e99ac08bf7c928eebd4241b43d09
F.Uhart <fabien.uhart@gmail.com>
2020-03-11 22:20:27 +0100

    last commit

---
+++ my_app/README_FabienUhart.md
@@ -0,0 +1,30 @@
+**Ma todo List:**
+
+Temps de réalisation:
+    2 Jours et demi
+
+Utilisation:
+    Le Projet etait de réaliser des todos listes avec un délai de temps
+    Vous pourrez voir que j'ai utilisé differents packages pour la gestion des dates et du datepicker
+    "moment": "^2.24.0",
+    "moment-timezone": "^0.5.28",
+    "react": "^16.13.0",
+    "react-datepicker": "^2.14.0",
+    "react-dom": "^16.13.0",
+    "react-moment": "^0.9.7",
+    "react-scripts": "^3.4.0"
+
+Projet à long terme :
+	La todo liste changent de couleur (font vert) lorqu'elle est validée
+	Rajout d'un dégradé de vert au rouge au titre de la todo pour définir un rapport avec la date approchant
+	Rajout d'un bouton pour dire que tel action est en réalisation, rajout d'un décompte en rapport au temps estimé
+	Rajout de graphiques avec les todos, pour savoir si l'on est bon en estimation
+
+Problématiques:
+	Je suis parti d'un tutoriel en anglais qui n'a pas abouti:
+		https://www.youtube.com/watch?v=sBws8MSXN7A&fbclid=IwAR3QasIbxNmwrEDqmxLOR-NFyE_6RpyH2RPYMf5KUYrmj6sLIPJiy3O3qog
+	- npm ne voulait pas se lancer
+	- la gestion des states en second lieu ne se faisait pas
+	
+	Il m'a été compliqué de gérer la structure de ReactJs et la gestion des dates
+	Il manque le changement de classe ou de style en rapport à une todo validé
--- my_app/src/Todo/TodoList.js
+++ my_app/src/Todo/TodoList.js
@@ -1,75 +1,204 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Moment from "react-moment";
+import moment from "moment";
+import "moment-timezone";
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+function EventValid(props) {
+  return <h6>Validation</h6>;
+}
+
+function EventNoValid(props) {
+  return <h6>wainting vallidation</h6>;
+}
+
+function Greeting(props) {
+  console.log(props);
+  const isLoggedIn = props[3];
+  if (isLoggedIn) {
+    return <EventValid />;
+  }
+  return <EventNoValid />;
+}
 
 class TodoList extends Component {
-    constructor() {
-        super();
-        this.state = {
-            userInput: '',
-            items: []
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      time: Date.now(),
+      userInput: "",
+      stateTodo: false,
+      startDate: new Date(),
+      endDate: new Date(),
+      items: []
+    };
+  }
 
-    onChange(event) {
-        this.setState({
-            userInput: event.target.value
-        });
-    }
+  onChange(event) {
+    this.setState({
+      userInput: event.target.value
+    });
+  }
 
-    addTodo(event) {
-        event.preventDefault();
-        this.setState({
-            userInput: '',
-            items: [...this.state.items, this.state.userInput]
-        });
-    }
+  addTodo(event) {
+    event.preventDefault();
+    let dateTimestamp = moment(new Date()).format("DD/MM/YYYY HH:mm:ss");
+    this.setState({
+      userInput: "",
+      dateEvent: "",
+      items: [
+        ...this.state.items,
+        [
+          this.state.userInput,
+          this.state.startDate,
+          this.state.endDate,
+          this.state.stateTodo,
+          dateTimestamp
+        ]
+      ]
+    });
+    this.setState({
+      startDate : new Date(),
+      endDate : this.state.startDate,
+    });
 
-    deleteTodo(item) {
-        // no event 
-        // pass the item to indexOf
-        const array = this.state.items;
-        const index = array.indexOf(item);
-        array.splice(index, 1);
-        this.setState({
-            items: array
-        });
-    }
+  }
 
-    // add item to deleteTodo.bind(this, item)
-    renderTodos() {
-        return this.state.items.map((item) => {
-            return (
-                <div className="list-group-item" key={item}>
-                    {item} | <button onClick={this.deleteTodo.bind(this, item)}>X</button>
-                </div>    
-            );
-        });
-    }
+  deleteTodo(item) {
+    const array = this.state.items;
+    const index = array.indexOf(item);
+    array.splice(index, 1);
+    this.setState({
+      items: array
+    });
+  }
 
-    render() {
-        return(
-            <div>
-                <h1 align="center">Ma Todo list</h1>
-                <form className="form-row align-items-center">
-                    <input 
-                        value={this.state.userInput} 
-                        type="text" 
-                        placeholder="Renseigner un item"
-                        onChange={this.onChange.bind(this)}
-                        className="form-control mb-2"
-                    />
-                    <button 
-                        onClick={this.addTodo.bind(this)} 
-                        className="btn btn-primary"
-                    >
-                        Ajouter
-                    </button>
-                </form>
-                <div className="list-group">
-                    {this.renderTodos()}
-                </div>
-            </div>
-        );
+  changeTodo(item) {
+    const array = this.state.items;
+    const index = array.indexOf(item);
+    const newItem = item;
+    newItem[3] = true;
+    array.splice(index, 1, newItem);
+    this.setState({
+      items: array
+    });
+  }
+
+  renderTodos() {
+    return this.state.items.map(item => {
+      let title = item[0];
+      let startDate = item[1];
+      let endDate = item[2];
+      let stateTodo = item[3]
+      let currentdate = item[4];
+      return (
+        <div className="list-group-item" key={item}>
+          <Greeting isLoggedIn={item} />
+          <div>
+            <button onClick={this.deleteTodo.bind(this, item)}>X</button>
+          </div>
+          <div>
+            <h2>{title} :</h2><Moment format="DD/MM/YYYY HH:mm:ss" withTitle>
+              {currentdate}
+            </Moment>
+          </div>
+          <div>
+            Date de début:
+            <Moment format="DD/MM/YYYY HH:mm" withTitle>
+              {startDate}
+            </Moment>
+          </div>
+          <div>
+            Date de fin:
+            <Moment format="DD/MM/YYYY HH:mm" withTitle>
+              {endDate}
+            </Moment>
+          </div>
+          <div>
+            <button onClick={this.changeTodo.bind(this, item)}>Valider</button>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  handleChange = date => {
+    this.setState({
+      startDate: date,
+      endDate: date
+    });
+  };
+
+  newHandleChange = date => {
+    if (this.state.startDate.getTime() <= date.getTime()) {
+      this.setState({
+        endDate: date
+      });
+    } else {
+      this.setState({
+        endDate: this.state.startDate
+      });
     }
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(
+      () => this.setState({ time: Date.now() }),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 align="center">Ma Todo list</h1>
+        <div>
+          <Moment format="  DD/MM/YYYY HH:mm:ss" withTitle>
+            {this.state.time}
+          </Moment>
+        </div>
+        <form className="form-row align-items-center">
+          <input
+            value={this.state.userInput}
+            type="text"
+            placeholder="Renseigner un item"
+            onChange={this.onChange.bind(this)}
+            className="form-control mb-2"
+          />
+          <DatePicker
+            selected={this.state.startDate}
+            onChange={this.handleChange}
+            showTimeSelect
+            timeFormat="HH:mm"
+            timeIntervals={15}
+            timeCaption="time"
+            dateFormat="dd/MM/yyyy HH:mm"
+            minDate={this.state.startDate}
+          />
+          <DatePicker
+            selected={this.state.endDate}
+            onChange={this.newHandleChange}
+            showTimeSelect
+            timeFormat="HH:mm:ss"
+            timeIntervals={15}
+            timeCaption="time"
+            dateFormat="dd/MM/yyyy HH:mm"
+            minDate={this.state.endDate}
+          />
+          <button onClick={this.addTodo.bind(this)} className="btn btn-primary">
+            Ajouter
+          </button>
+        </form>
+        <div className="list-group">{this.renderTodos()}</div>
+      </div>
+    );
+  }
 }
 
 export default TodoList;
